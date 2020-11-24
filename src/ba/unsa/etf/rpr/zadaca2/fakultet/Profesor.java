469dbd2c38ba2170926e18a77c12f58eaa0ec7b1
mkapo3 <mkapo3@etf.unsa.ba>
2020-11-24 16:12:40 +0100

    Dodan konstruktor u klasi Profesor.

--- src/ba/unsa/etf/rpr/zadaca2/fakultet/Profesor.java
+++ src/ba/unsa/etf/rpr/zadaca2/fakultet/Profesor.java
@@ -9,5 +9,11 @@ public class Profesor {
     private List<Predmet> predmeti;
     private int norma;
 
-
+    public Profesor(String ime,String prezime, ArrayList<Predmet> predmeti) {
+        this.ime = ime;
+        this.prezime=prezime;
+        this.predmeti=predmeti;
+        norma=predmeti.stream().map(Predmet::getBroj_casova_godisnje).
+                reduce(0 , (a,b) -> a+b);
+    }
 }
